960d3aa59619d630fcaf21faa846ec1d2ea9e79d
Dávid Miklós <miklosdavid12@gmail.com>
2026-08-14 12:30:23 +0300

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # solution 2 (hash compare)
+        if len(s) != len(t):
+            return False
+
+        count_s, count_t = {}, {}
+
+        for ch in s:
+            count_s[ch] = count_s.get(ch, 0) + 1
+        for ch in t:
+            count_t[ch] = count_t.get(ch, 0) + 1
+        
+        return count_s == count_t
+
+        # ---
+
+        # # solution 2 (hash - array)
+        # if len(s) != len(t):
+        #     return False
+
+        # count = [0] * 26
+        # ord_a = ord("a")
+
+        # for ch in s:
+        #     count[ord(ch) - ord_a] += 1
+        # for ch in t:
+        #     count[ord(ch) - ord_a] -= 1
+
+        # return all(c == 0 for c in count)
+
+        # ---
+
+        # # solution 1 (sort)
+        # if len(s) != len (t):
+        #     return False
+        # return sorted(s) == sorted(t)
