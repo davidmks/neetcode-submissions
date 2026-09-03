c44a2aac51f35a9670c04db3d5b94dbe5f842287
Dávid Miklós <miklosdavid12@gmail.com>
2026-09-03 10:15:15 +0300

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+        
+        fleets = 0
+        prev_time = float(0)
+        
+        for pos, speed in cars:
+            time = (target - pos) / speed
+            if time > prev_time:
+                fleets += 1
+                prev_time = time
+            
+        return fleets
